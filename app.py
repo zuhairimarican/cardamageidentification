32c51385013914133ea94b66289a0ce8fb231a3f
SuGit-Demo <89434151+SuGit-Demo@users.noreply.github.com>
2022-01-18 15:35:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import Flask, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
-
 app = Flask(__name__)
 
 from keras.models import load_model 
