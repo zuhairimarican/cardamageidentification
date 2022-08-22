42800b9528b6eeed4f5c8d007fc6bfcd67fefdfb
zuhairimarican <111715996+zuhairimarican@users.noreply.github.com>
2022-08-22 14:04:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ print("Loading model")
 #sess = tf.compat.v1.Session()
 #set_session(sess)
 global model 
-model = load_model('AppleOrangeRGCWorking.h5') 
+model = load_model('AppleOrangeRGBWorking.h5') 
 #global graph
 #graph = tf.compat.v1.get_default_graph()
 
